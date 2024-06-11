3040c14615b6e4977379d50d028a39d37fda758a
Slava <slavok.v005@mail.ru>
2024-06-11 18:30:59 +0200

    aaa

--- SQL/HW_12.06/hw.sql
+++ SQL/HW_12.06/hw.sql
@@ -1,3 +1,23 @@
+ 1 уровень сложности: В рамках БД "песочница: интернет-магазин":
+
+Вывести товары, добавив поле цена со наценкой Price_up в случае, если:
+
+цена от 200 EUR — наценка 0.5%
+
+цена от 20 EUR — наценка 1.5%
+SELECT 
+    ProductID,
+    ProductName,
+    Price,
+    CASE 
+        WHEN Price >= 200 THEN Price * 1.005
+        WHEN Price >= 20 THEN Price * 1.015
+        ELSE Price
+    END AS Price_up
+FROM 
+    Products;
+    
+//----------------------------
 CREATE TABLE products (
     product_id int primary key auto_increment,
     name varchar(128) not null,
